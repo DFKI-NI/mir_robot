2b5e01d8d6d34380bd69cc86ceecd35ffe3fb51a
niniemann <nils.niemann@dfki.de>
2020-02-18 09:36:38 +0100

    Add optional prefix parameter to fake_mir_joint_publisher (#47)

--- mir_driver/nodes/fake_mir_joint_publisher.py
+++ mir_driver/nodes/fake_mir_joint_publisher.py
@@ -4,16 +4,17 @@ from sensor_msgs.msg import JointState
 
 def fake_mir_joint_publisher():
     rospy.init_node('fake_mir_joint_publisher')
+    prefix = rospy.get_param('~prefix', '')
     pub = rospy.Publisher('joint_states', JointState, queue_size=10)
     r = rospy.Rate(50.0)
     while not rospy.is_shutdown():
         js = JointState()
         js.header.stamp = rospy.Time.now()
-        js.name =  ['left_wheel_joint', 'right_wheel_joint',
-                    'fl_caster_rotation_joint', 'fl_caster_wheel_joint',
-                    'fr_caster_rotation_joint', 'fr_caster_wheel_joint',
-                    'bl_caster_rotation_joint', 'bl_caster_wheel_joint',
-                    'br_caster_rotation_joint', 'br_caster_wheel_joint']
+        js.name =  [prefix + 'left_wheel_joint', prefix + 'right_wheel_joint',
+                    prefix + 'fl_caster_rotation_joint', prefix + 'fl_caster_wheel_joint',
+                    prefix + 'fr_caster_rotation_joint', prefix + 'fr_caster_wheel_joint',
+                    prefix + 'bl_caster_rotation_joint', prefix + 'bl_caster_wheel_joint',
+                    prefix + 'br_caster_rotation_joint', prefix + 'br_caster_wheel_joint']
         js.position = [0.0 for _ in js.name]
         js.velocity = [0.0 for _ in js.name]
         js.effort = [0.0 for _ in js.name]
