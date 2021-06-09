9e2b74dc1b0363822b0c0039a38873021951e4a5
Martin Günther <martin.guenther@dfki.de>
2021-06-09 17:42:40 +0200

    Remove outdated topics
    These topics don't exist on MiR software 2.8.3 any more (most of them
    have been removed a long time ago).
    
    Fixes #37.

--- mir_driver/nodes/mir_bridge.py
+++ mir_driver/nodes/mir_bridge.py
@@ -134,12 +134,8 @@ PUB_TOPICS = [
               TopicConfig('camera_floor/depth/parameter_descriptions', ConfigDescription),
               TopicConfig('camera_floor/depth/parameter_updates', Config),
               TopicConfig('camera_floor/depth/points', PointCloud2),
-              TopicConfig('camera_floor/filter/parameter_descriptions', ConfigDescription),
-              TopicConfig('camera_floor/filter/parameter_updates', Config),
               TopicConfig('camera_floor/floor', PointCloud2),
               TopicConfig('camera_floor/obstacles', PointCloud2),
-              TopicConfig('camera_floor/transform/parameter_descriptions', ConfigDescription),
-              TopicConfig('camera_floor/transform/parameter_updates', Config),
               TopicConfig('check_area/polygon', PolygonStamped),
               TopicConfig('diagnostics', DiagnosticArray),
               TopicConfig('diagnostics_agg', DiagnosticArray),
@@ -149,26 +145,18 @@ PUB_TOPICS = [
               TopicConfig('imu_data', Imu),  # not available in simulation
               TopicConfig('laser_back/driver/parameter_descriptions', ConfigDescription),
               TopicConfig('laser_back/driver/parameter_updates', Config),
-              TopicConfig('laser_back/transform/parameter_descriptions', ConfigDescription),
-              TopicConfig('laser_back/transform/parameter_updates', Config),
               TopicConfig('laser_front/driver/parameter_descriptions', ConfigDescription),
               TopicConfig('laser_front/driver/parameter_updates', Config),
-              TopicConfig('laser_front/transform/parameter_descriptions', ConfigDescription),
-              TopicConfig('laser_front/transform/parameter_updates', Config),
               TopicConfig('map', OccupancyGrid, latch=True),
               TopicConfig('map_metadata', MapMetaData),
               TopicConfig('mir_amcl/parameter_descriptions', ConfigDescription),
               TopicConfig('mir_amcl/parameter_updates', Config),
               TopicConfig('mir_amcl/selected_points', PointCloud2),
               TopicConfig('mir_log', Log),
-              TopicConfig('mir_serial_button', Serial),
-              TopicConfig('mir_sound', String),
               TopicConfig('mir_status_msg', String),
 #              TopicConfig('mirspawn/node_events', LaunchItem),
               TopicConfig('mirwebapp/grid_map_metadata', LocalMapStat),
               TopicConfig('mirwebapp/laser_map_metadata', LocalMapStat),
-##              TopicConfig('move_base/feedback', MirMoveBaseActionFeedback),
-##              TopicConfig('move_base/result', MirMoveBaseActionResult),
               TopicConfig('move_base/feedback', MoveBaseActionFeedback, dict_filter=_move_base_feedback_dict_filter),  # really mir_actions/MirMoveBaseActionFeedback
               TopicConfig('move_base/result',   MoveBaseActionResult,   dict_filter=_move_base_result_dict_filter),    # really mir_actions/MirMoveBaseActionResult
               TopicConfig('move_base/status', GoalStatusArray),
@@ -199,16 +187,7 @@ PUB_TOPICS = [
 #              TopicConfig('move_base_node/mir_escape_recovery/visualization_marker', Marker),
 #              TopicConfig('move_base_node/parameter_descriptions', ConfigDescription),
 #              TopicConfig('move_base_node/parameter_updates', Config),
-              TopicConfig('odom_comb', Odometry),    # odom_comb on real robot, odom on simulator
               TopicConfig('odom_enc', Odometry),
-#              TopicConfig('particlecloud', PoseArray),
-#              TopicConfig('relative_move_action/feedback', RelativeMoveActionFeedback),
-#              TopicConfig('relative_move_action/result', RelativeMoveActionResult),
-#              TopicConfig('relative_move_action/status', GoalStatusArray),
-#              TopicConfig('relative_move_node/parameter_descriptions', ConfigDescription),
-#              TopicConfig('relative_move_node/parameter_updates', Config),
-#              TopicConfig('relative_move_node/time_to_coll', Float64),
-#              TopicConfig('relative_move_node/visualization_marker', Marker),
               TopicConfig('robot_mode', RobotMode),
               TopicConfig('robot_pose', Pose),
               TopicConfig('robot_state', RobotState),
@@ -217,11 +196,7 @@ PUB_TOPICS = [
               TopicConfig('scan', LaserScan),
               TopicConfig('scan_filter/visualization_marker', Marker),
               TopicConfig('tf', tfMessage, dict_filter=_tf_dict_filter),
-              TopicConfig('tf_static', tfMessage, dict_filter=_tf_dict_filter),
-              TopicConfig('transform_footprint/parameter_descriptions', ConfigDescription),
-              TopicConfig('transform_footprint/parameter_updates', Config),
-              TopicConfig('transform_imu/parameter_descriptions', ConfigDescription),
-              TopicConfig('transform_imu/parameter_updates', Config)]
+              TopicConfig('tf_static', tfMessage, dict_filter=_tf_dict_filter)]
 
 # topics we want to subscribe to from ROS (and publish to the MiR)
 SUB_TOPICS = [TopicConfig('cmd_vel', Twist, dict_filter=_cmd_vel_dict_filter),
@@ -229,11 +204,7 @@ SUB_TOPICS = [TopicConfig('cmd_vel', Twist, dict_filter=_cmd_vel_dict_filter),
               TopicConfig('light_cmd', String),
               TopicConfig('mir_cmd', String),
               TopicConfig('move_base/cancel', GoalID),
-##              TopicConfig('move_base/goal', MirMoveBaseActionGoal),
               TopicConfig('move_base/goal', MoveBaseActionGoal, dict_filter=_move_base_goal_dict_filter)]  # really mir_actions/MirMoveBaseActionGoal
-#              TopicConfig('move_base_simple/goal', PoseStamped)]
-#              TopicConfig('relative_move_action/cancel', GoalID),
-#              TopicConfig('relative_move_action/goal', RelativeMoveActionGoal)]
 
 class PublisherWrapper(rospy.SubscribeListener):
     def __init__(self, topic_config, robot):
