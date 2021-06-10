d284be4b44e73cc6be65fbc131f219683a471559
Martin Günther <martin.guenther@dfki.de>
2021-06-10 10:29:18 +0200

    Add pre-commit config

---
+++ .mdl_style.rb
@@ -0,0 +1,21 @@
+# Style file for markdownlint
+
+all
+
+exclude_rule 'fenced-code-language' # Fenced code blocks should have a language specified
+exclude_rule 'first-line-h1' # First line in file should be a top level header
+
+exclude_rule 'ul-style'
+
+exclude_rule 'no-multiple-blanks'
+
+exclude_rule 'header-style'
+
+# Line lenght
+rule 'MD013', :line_length => 120, :code_blocks => false, :tables => false
+
+# Unordered list indentation
+rule 'MD007', :indent => 2
+
+# Ordered list item prefix
+rule 'MD029', :style => 'ordered'
