c10857b8eee16b69a64cc2d21fd2a74d3f11074f
Matthias Mayr <matthias.mayr@cs.lth.se>
2022-09-08 18:04:26 +0000

    Fix: Prevent fake joint publisher from TF_REPEATED_DATA
    - During startup it can happen that the clock does not increase
    - In some configuration Gazebo also published data for the wheel
      leading to collisions with this publisher creating these msgs:
    
    "TF_REPEATED_DATA ignoring data with redundant timestamp for
    frame mir/left_wheel_link at time 45.782000"

--- mir_driver/nodes/fake_mir_joint_publisher.py
+++ mir_driver/nodes/fake_mir_joint_publisher.py
@@ -7,22 +7,32 @@ def fake_mir_joint_publisher():
     rospy.init_node('fake_mir_joint_publisher')
     prefix = rospy.get_param('~prefix', '')
     pub = rospy.Publisher('joint_states', JointState, queue_size=10)
+    publish_wheel_joints = rospy.get_param('~publish_wheel_joints', True)
     r = rospy.Rate(50.0)
+    t = None
     while not rospy.is_shutdown():
         js = JointState()
         js.header.stamp = rospy.Time.now()
-        js.name = [
-            prefix + 'left_wheel_joint',
-            prefix + 'right_wheel_joint',
-            prefix + 'fl_caster_rotation_joint',
-            prefix + 'fl_caster_wheel_joint',
-            prefix + 'fr_caster_rotation_joint',
-            prefix + 'fr_caster_wheel_joint',
-            prefix + 'bl_caster_rotation_joint',
-            prefix + 'bl_caster_wheel_joint',
-            prefix + 'br_caster_rotation_joint',
-            prefix + 'br_caster_wheel_joint',
-        ]
+        if js.header.stamp == t:
+            rospy.loginfo("Timestamp identical: %s, %s", js.header.stamp, t)
+            r.sleep()
+            continue
+
+        t = js.header.stamp
+        js.name = [prefix + 'fl_caster_rotation_joint',
+                   prefix + 'fl_caster_wheel_joint',
+                   prefix + 'fr_caster_rotation_joint',
+                   prefix + 'fr_caster_wheel_joint',
+                   prefix + 'bl_caster_rotation_joint',
+                   prefix + 'bl_caster_wheel_joint',
+                   prefix + 'br_caster_rotation_joint',
+                   prefix + 'br_caster_wheel_joint',
+                   ]
+        if (publish_wheel_joints):
+            js.name.extend([
+                prefix + 'left_wheel_joint',
+                prefix + 'right_wheel_joint',
+            ])
         js.position = [0.0 for _ in js.name]
         js.velocity = [0.0 for _ in js.name]
         js.effort = [0.0 for _ in js.name]
