c86700832706cea881eceb9c68a5417bf2aa652a
Rajath <rajatha@chalmers.se>
2023-04-08 16:26:16 +0200

    Simulation final commit , Real robot first commit

--- mir_driver/nodes/fake_mir_joint_publisher.py
+++ mir_driver/nodes/fake_mir_joint_publisher.py
@@ -36,7 +36,7 @@ from sensor_msgs.msg import JointState
 def fake_mir_joint_publisher():
     rospy.init_node('fake_mir_joint_publisher')
     prefix = rospy.get_param('~prefix', '')
-    pub = rospy.Publisher('joint_states', JointState, queue_size=10)
+    pub = rospy.Publisher('mir_joint_states', JointState, queue_size=10)
     r = rospy.Rate(50.0)
     while not rospy.is_shutdown():
         js = JointState()
